fb0cf2d056704e87ef1ef27629aad192813ad164
Guilherme Funchal da Silva <guilherme.funchal@gmail.com>
2022-10-18 22:26:12 -0300

    Create Modal3.js

---
+++ modal/src/Modal/Modal3.js
@@ -0,0 +1,120 @@
+import React, { useReducer, useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+import { CSSTransition } from "react-transition-group";
+//import "./Modal.css";
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from 'react-bootstrap/Row';
+import { Controller, useForm } from "react-hook-form";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const Modal = props => {
+  // const closeOnEscapeKeyDown = e => {
+  //   if ((e.charCode || e.keyCode) === 27) {
+  //     props.onClose();
+  //   }
+  // };
+  const form = useRef(null);
+  const [validated, setValidated] = useState(false);
+  const [firstname, setFirstname] = useState(" ");
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      firstName: "",
+    },
+  });
+  
+  const handlesubmit = () => {
+    form.current.reset(); //this will reset all the inputs in the form
+  }
+
+  function resetForm() {
+    document.getElementById("form").reset();
+  }
+
+
+  const submitForm = (data) => {
+
+    console.log(data);
+    form.current.reset(); 
+    props.onClose();
+  };
+
+  // const handleSubmit = (event) => {
+
+  //   const form = event.currentTarget;
+  //   console.log(form.checkValidity())
+  //   if (form.checkValidity() === false) {
+  //     event.preventDefault();
+  //     event.stopPropagation();
+  //   }
+
+  //   setValidated(true);
+  // };
+
+  useEffect(() => {
+    // document.body.addEventListener("keydown", closeOnEscapeKeyDown);
+    // return function cleanup() {
+    //   document.body.removeEventListener("keydown", closeOnEscapeKeyDown);
+    // };
+  }, []);
+
+  return ReactDOM.createPortal(
+    <CSSTransition
+      in={props.show}
+      // unmountOnExit
+      // timeout={{ enter: 0, exit: 300 }}
+    >
+      <div className="modal" onClick={props.onClose}>
+        <div className="modal-content" onClick={e => e.stopPropagation()}>
+          <div className="modal-header">
+            <h4 className="modal-title">{props.title}</h4>
+          </div>
+          <div className="modal-body">
+          <Form ref={form} noValidate onSubmit={handleSubmit(submitForm)}>
+          <Row className="mb-3">
+            <Form.Group as={Col} md="4" controlId="validationCustom01">
+              <Form.Label>First Name</Form.Label>
+              <Controller
+                name="firstName"
+                control={control}
+                onChange={(e) => setFirstname(e.target.value)}
+                rules={{ required: true }}
+                render={({ field }) => (
+                  <Form.Control
+                    {...field}
+                    type="text"
+                    placeholder="First name"
+                    isInvalid={errors.firstName}
+                  />
+                )}
+              />
+              {errors.firstName && (
+                 <div class="invalid-feedback">
+                <Form.Control.Feedback type="invalid">
+                  First Name is required
+                </Form.Control.Feedback>
+                </div>
+              )}
+            </Form.Group>
+          </Row>
+          <Button type="submit">Submit form</Button>
+
+        </Form>
+          </div>
+        </div>
+      </div>
+      
+    </CSSTransition>,
+    document.getElementById("root")
+  );
+};
+
+export default Modal;
