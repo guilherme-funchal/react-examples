d0aabddd6e3b648fc4132721e16eb4e79629fc5d
Guilherme Funchal da Silva <guilherme.funchal@gmail.com>
2022-11-05 19:39:38 -0300

    Update app.js

--- rest/app.js
+++ rest/app.js
@@ -26,11 +26,44 @@ app.post('/user/add', (req, res) => {
     saveUserData(existUsers);
     res.send({success: true, msg: 'User data added successfully'})
 })
+
+app.post('/user-lista/add', (req, res) => {
+    //get the existing user data
+    const existUsers = getUserDataLista()
+    
+    //get the new user data from post request
+    const userData = req.body
+    //check if the userData fields are missing
+    if (userData.user_id == null) {
+        return res.status(401).send({error: true, msg: 'User data missing'})
+    }
+    
+    //check if the username exist already
+    const findExist = existUsers.find( user => user.user_id === userData.user_id )
+    if (findExist) {
+        return res.status(409).send({error: true, msg: 'username already exist'})
+    }
+    //append the user data
+    existUsers.push(userData)
+    //save the new user data
+    saveUserDataLista(existUsers);
+    res.send({success: true, msg: 'User data added successfully'})
+})
 /* Read - GET method */
 app.get('/user/list', (req, res) => {
     const users = getUserData()
     res.send(users)
 })
+app.get('/user-lista/list', (req, res) => {
+    const users = getUserDataLista()
+    res.send(users)
+})
+
+app.get('/user-lista/id', (req, res) => {
+    const users = getUserDataLista()
+    res.send(users[0].user_id)
+})
+
 /* Update - Patch method */
 app.patch('/user/update/:user_id', (req, res) => {
     //get the username from url
@@ -75,6 +108,22 @@ app.delete('/user/delete/:user_id', (req, res) => {
     res.send({success: true, msg: 'User removed successfully'})
     
 })
+
+app.delete('/user-lista/delete/:user_id', (req, res) => {
+    const user_id = req.params.user_id
+    //get the existing userdata
+    const existUsers = getUserDataLista()
+    //filter the userdata to remove it
+    const filterUser = existUsers.filter( user => user.user_id !== user_id )
+    if ( existUsers.length === filterUser.length ) {
+        return res.status(409).send({error: true, msg: 'username does not exist'})
+    }
+    //save the filtered data
+    saveUserDataLista(filterUser)
+    res.send({success: true, msg: 'User removed successfully'})
+    
+})
+
 /* util functions */
 //read the user data from json file
 const saveUserData = (data) => {
@@ -87,6 +136,18 @@ const getUserData = () => {
     const jsonData = fs.readFileSync('users.json')
     return JSON.parse(jsonData)    
 }
+/* util functions */
+//read the user data from json file
+const saveUserDataLista = (data) => {
+    const stringifyData = JSON.stringify(data)
+    fs.writeFileSync('users-lista.json', stringifyData)	
+}
+//get the user data from json file
+const getUserDataLista = () => {
+    const jsonData = fs.readFileSync('users-lista.json')
+    return JSON.parse(jsonData)    
+}
+
 /* util functions ends */
 //configure the server port
 app.listen(3001, () => {
