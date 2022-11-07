948632d76092fde1b71985a4b68008f537f1db93
Guilherme Funchal da Silva <guilherme.funchal@gmail.com>
2022-11-07 20:25:21 -0300

    Update Modal1.js

--- modal/src/Modal/Modal1.js
+++ modal/src/Modal/Modal1.js
@@ -1,27 +1,62 @@
-import React, { useEffect } from "react";
+import React, { useReducer, useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 import { CSSTransition } from "react-transition-group";
-import "./Modal.css";
+//import "./Modal.css";
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from 'react-bootstrap/Row';
+import { Controller, useForm } from "react-hook-form";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Modal = props => {
-  const closeOnEscapeKeyDown = e => {
-    if ((e.charCode || e.keyCode) === 27) {
-      props.onClose();
-    }
+
+function Modal(props) {
+  // const closeOnEscapeKeyDown = e => {
+  //   if ((e.charCode || e.keyCode) === 27) {
+  //     props.onClose();
+  //   }
+  // };
+  console.log(props.name);
+  
+  const form = useRef(null);
+  const [validated, setValidated] = useState(false);
+  const [firstname, setFirstname] = useState(" ");
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      firstName: "",
+    },
+  });
+  
+  const handlesubmit = () => {
+    form.current.reset(); //this will reset all the inputs in the form
+  }
+
+  function resetForm() {
+    document.getElementById("form").reset();
+  }
+
+
+  const submitForm = (data) => {
+    form.current.reset(); 
+    props.onClose();
   };
 
   useEffect(() => {
-    document.body.addEventListener("keydown", closeOnEscapeKeyDown);
-    return function cleanup() {
-      document.body.removeEventListener("keydown", closeOnEscapeKeyDown);
-    };
+    console.log(props.name);
+    const teste = props.name;
   }, []);
 
   return ReactDOM.createPortal(
     <CSSTransition
       in={props.show}
-      unmountOnExit
-      timeout={{ enter: 0, exit: 300 }}
+      // unmountOnExit
+      // timeout={{ enter: 0, exit: 300 }}
     >
       <div className="modal" onClick={props.onClose}>
         <div className="modal-content" onClick={e => e.stopPropagation()}>
@@ -29,15 +64,40 @@ const Modal = props => {
             <h4 className="modal-title">{props.title}</h4>
           </div>
           <div className="modal-body">
-            teste1
-          </div>
-          <div className="modal-footer">
-            <button onClick={props.onClose} className="button">
-              Close1
-            </button>
+          <Form ref={form} noValidate onSubmit={handleSubmit(submitForm)}>
+          <Row className="mb-3">
+            <Form.Group as={Col} md="4" controlId="validationCustom01">
+              <Form.Label>First Name</Form.Label>
+              <Controller
+                name="firstName"
+                control={control}
+                onChange={(e) => setFirstname(e.target.value)}
+                rules={{ required: true }}
+                render={({ field }) => (
+                  <Form.Control
+                    {...field}
+                    type="text"
+                    placeholder="First name"
+                    isInvalid={errors.firstName}
+                  />
+                )}
+              />
+              {errors.firstName && (
+                 <div class="invalid-feedback">
+                <Form.Control.Feedback type="invalid">
+                  First Name is required
+                </Form.Control.Feedback>
+                </div>
+              )}
+            </Form.Group>
+          </Row>
+          <Button type="submit">Submit form</Button>
+
+        </Form>
           </div>
         </div>
       </div>
+      
     </CSSTransition>,
     document.getElementById("root")
   );
